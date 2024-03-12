1d1fbb4e09d223165005b13dbbd4bb4605d7781f
Jon D <info@darkjon.de>
2024-03-12 13:00:28 +0100

    .
    .

---
+++ mario.py
@@ -0,0 +1,35 @@
+import numpy as np
+import sys
+from handwriting_synthesis import Hand
+
+Test1 = sys.argv[1]
+Test2 = sys.argv[2]
+Test3 = sys.argv[3]
+
+print (Test1 + Test2 + Test3)
+
+all_star = """Hier steht das erste Argv : """ + Test1 + """
+Hier steht das zweite Argv : """ + Test2 + """
+Hier steht das dritte Argv : """ + Test3
+
+lines = [
+    "Father time, I'm running late",
+    "I'm winding down, I'm growing tired",
+    "Seconds drift into the night",
+    "The clock just ticks till my time expires",
+]
+
+if __name__ == '__main__':
+    hand = Hand()
+
+    # demo number 1 - fixed bias, fixed style
+    lines = all_star.split("\n")
+    biases = [.75 for i in lines]
+    styles = [12 for i in lines]
+
+    hand.write(
+        filename='img/mario.svg',
+        lines=lines,
+        biases=biases,
+        styles=styles,
+    )
